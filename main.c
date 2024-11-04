4fdba26c967f17f293ae7e82aa420445f4fa9d5a
sikehish <hisham0502@gmail.com>
2024-11-04 12:03:23 +0530

    Tokenization logic in place

--- main.c
+++ main.c
@@ -1,7 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 typedef enum TokenType{ TOKEN_PLUS, TOKEN_SUB, TOKEN_MUL, TOKEN_DIV, TOKEN_NUM, TOKEN_END} TokenType;
 
-struct Token{
+typedef struct Token{
     TokenType type;
     int value;
-};
+} Token;
 
+  typedef struct Result{
+        TokenType type;
+        int isOperator ;
+    } Result;
+
+void checkChar(char c, struct Result* result) {
+    switch (c) {
+        case '+':
+            result->type = TOKEN_PLUS; 
+            result->isOperator = 1; 
+            break;
+        case '-':
+            result->type = TOKEN_SUB; 
+            result->isOperator = 1; 
+            break;
+        case '*':
+            result->type = TOKEN_MUL; 
+            result->isOperator = 1; 
+            break;
+        case '/':
+            result->type = TOKEN_DIV; 
+            result->isOperator = 1; 
+            break;
+        default: 
+            result->isOperator = 0; 
+            break;
+    }
+}
+
+// Tokenization
+Token* tokenize(char* expression){
+    Token* tokens = malloc(256 * sizeof(Token));
+    if (!tokens) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    
+    int i = 0;
+    struct Result checkResult;
+
+    while (*expression) {
+        checkChar(*expression, &checkResult);
+
+        if (checkResult.isOperator) {
+            tokens[i].type = checkResult.type;
+            tokens[i].value = -1; 
+            i++;
+            expression++;
+        } else if (isdigit(*expression)) {
+            tokens[i].type = TOKEN_NUM;
+            tokens[i].value = strtol(expression, &expression, 10);
+            i++;
+        } else expression++;
+        
+    }
+
+    tokens[i].type = TOKEN_END;
+    tokens[i].value = -1;
+    return tokens;
+}
+
+// Parsing
+
+
+// Code gen
